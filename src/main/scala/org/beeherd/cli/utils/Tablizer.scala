abcdf4489209d9b59c01cb9b56d566356992d0bf
Samuel Cox <crankydill@gmail.com>
2010-10-10 07:12:05 -0500

    Add license.

--- src/main/scala/org/beeherd/cli/utils/Tablizer.scala
+++ src/main/scala/org/beeherd/cli/utils/Tablizer.scala
@@ -1,3 +1,22 @@
+/**
+* Licensed to the Apache Software Foundation (ASF) under one
+* or more contributor license agreements. See the NOTICE file
+* distributed with this work for additional information
+* regarding copyright ownership. The ASF licenses this file
+* to you under the Apache License, Version 2.0 (the
+* "License"); you may not use this file except in compliance
+* with the License. You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied. See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
+
 package org.beeherd.cli.utils
 
 import scala.collection.mutable.ListBuffer
@@ -5,6 +24,8 @@ import scala.collection.mutable.ListBuffer
 /** 
 * This class is responsible for taking data and creating rows of Strings that
 * can be used to pretty print tables on the console.
+*
+* @author scox
 */
 class Tablizer(val pad: String) {
   type Row = List[String]
